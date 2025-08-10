1cef158a827dc5f1df74eca40f8df0df2275da90
Chhavi404 <chhavimendiratta.1@gmail.com>
2025-08-10 12:13:28 +0000

    CONVERSION

---
+++ basics/day2datatypes.js
@@ -0,0 +1,13 @@
+//classes,modules,arrow function are now not earlier
+//"use strict";//treat all js code as newer version//
+//alert("hello")//also used for arithmetic ops
+let score="Chhavi"
+console.log(typeof score)
+let convertintoNumber = Number(score)
+console.log(typeof convertintoNumber)
+console.log(convertintoNumber)
+let isLoggedIn = " "//it shud not be empty even a space will return true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);//true
+
+
